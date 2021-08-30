2ae7b2b46c5be9d28c3cb688b90ca41758f75c9a
gyeong <gyeongyeongi22@gmail.com>
2021-08-30 15:26:44 +0900

    push 확인

---
+++ app/src/main/java/com/corporation8793/aivision/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.corporation8793.aivision
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(
+            R.id.fragment,
+            HomeFragment()
+        )
+
+
+        transaction.commit()
+    }
+}
---
+++ app/src/main/java/com/corporation8793/aivision/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.corporation8793.aivision
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.timer
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+
+}
