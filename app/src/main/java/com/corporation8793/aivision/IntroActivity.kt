012f818e098c12476d9d94a183910f6c159313ea
gyeong <gyeongyeongi22@gmail.com>
2021-10-27 17:26:17 +0900

    intro indicator 추가

--- app/src/main/java/com/corporation8793/aivision/IntroActivity.kt
+++ app/src/main/java/com/corporation8793/aivision/IntroActivity.kt
@@ -1,11 +1,16 @@
 package com.corporation8793.aivision
 
 import android.content.Intent
+import android.gesture.GestureOverlayView.ORIENTATION_HORIZONTAL
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Adapter
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager.widget.ViewPager
+import com.tbuonomo.viewpagerdotsindicator.SpringDotsIndicator
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
 
 class IntroActivity  : AppCompatActivity() {
     // push 확인
@@ -14,27 +19,45 @@ class IntroActivity  : AppCompatActivity() {
         setContentView(R.layout.activity_intro)
 
 
-        var viewpager :ViewPager2 = findViewById(R.id.intro_viewpager)
-        viewpager.adapter = ViewPagerAdapter(getIntroList())
-        viewpager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        var viewpager :ViewPager = findViewById(R.id.intro_viewpager)
+        var adapter : ViewPagerAdapter = ViewPagerAdapter(getIntroList());
+        viewpager.adapter = adapter
+
+        var wormDotsIndicator  : WormDotsIndicator = findViewById<WormDotsIndicator>(R.id.spring_dots_indicator)
+        wormDotsIndicator .setViewPager(viewpager)
 
         var invisible_forever_btn : Button = findViewById(R.id.invisible_forever)
         var close_btn : Button = findViewById(R.id.close)
 
-        viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                if (position == 5){
-                    close_btn.isEnabled = true
-                    invisible_forever_btn.isEnabled = true
-                }else{
-                    close_btn.isEnabled = false
-                    invisible_forever_btn.isEnabled = false
+        viewpager?.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
 
-                }
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+            override fun onPageSelected(position: Int) {
+                Log.e("select","확인")
             }
 
         })
+
+//        viewpager.registerOnPageChangeCallback(object : ViewPager.OnPageChangeCallback(){
+//            override fun onPageSelected(position: Int) {
+//                super.onPageSelected(position)
+//                if (position == 5){
+//                    close_btn.isEnabled = true
+//                    invisible_forever_btn.isEnabled = true
+//                }else{
+//                    close_btn.isEnabled = false
+//                    invisible_forever_btn.isEnabled = false
+//
+//                }
+//            }
+//
+//        })
 
         invisible_forever_btn.setOnClickListener{
             Application.prefs.setBoolean("invisible", true)
--- app/src/main/java/com/corporation8793/aivision/ViewPagerAdapter.kt
+++ app/src/main/java/com/corporation8793/aivision/ViewPagerAdapter.kt
@@ -1,23 +1,54 @@
 package com.corporation8793.aivision
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.PagerAdapter
 
-class ViewPagerAdapter(introList: ArrayList<Int>) : RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
+class ViewPagerAdapter(introList: ArrayList<Int>) : PagerAdapter() {
     var item = introList
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
-
-    override fun getItemCount(): Int = item.size
-
-    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
-        holder.imageview.setImageResource(item[position])
+    fun ViewPagerAdapter(list : ArrayList<Int>){
+        item = list
     }
 
-    inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
-        (LayoutInflater.from(parent.context).inflate(R.layout.intro_list_item, parent, false)){
-            val imageview = itemView.findViewById<ImageView>(R.id.imageview)
+    //position에 해당하는 페이지 생성
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view=LayoutInflater.from(container.context).inflate(R.layout.intro_list_item,container,false)
+        val imageview = view.findViewById<ImageView>(R.id.imageview)
+        imageview.setImageResource(item[position])
+        container.addView(view)
+        return view
     }
+
+    //position에 위치한 페이지 제거
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View?)
+    }
+
+
+    //사용가능한 뷰 개수 리턴
+    override fun getCount(): Int {
+        return 6
+    }
+
+    //페이지뷰가 특정 키 객체(key object)와 연관 되는지 여부
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return (view==`object`)
+    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
+//
+//    override fun getItemCount(): Int = item.size
+//
+//    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+//        holder.imageview.setImageResource(item[position])
+//    }
+//
+//    inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
+//        (LayoutInflater.from(parent.context).inflate(R.layout.intro_list_item, parent, false)){
+//            val imageview = itemView.findViewById<ImageView>(R.id.imageview)
+//    }
 }
